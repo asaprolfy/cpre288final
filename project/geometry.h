1c2485718a2c13efb01dbaaba2063f67cefded2e
BJohnson98 <47310376+BJohnson98@users.noreply.github.com>
2019-04-18 16:34:14 -0500

    Create geometry.h
    Header file for geometry.c

---
+++ project/geometry.h
@@ -0,0 +1,20 @@
+#ifndef GEOMETRY_H
+#define GEOMETRY_H
+
+
+//finds midpoint between 2 points
+void midpoint(double *x, double *y, double x1, double y1, double x2, double y2);
+//finds a line perpindicular between 2 points
+void line_from_midpoint(double *slope, double *intercept, double x1, double y1, double x2, double y2);
+//finds the closest point from the robots starting position to the robot
+void point_from_robot(double slope, double intercept, double robot_x, double robot_y, double *x, double *y);
+//finds the position of an object using the angle and distance 
+void object_position(double x, double y, double angle, double distance, double *x_obj, double *y_obj);
+//return the distance between two points
+double distance_between_points(double x1, double y1, double x2, double y2);
+//gets the commands for the robot once everything has been calculated
+void get_commands(double *angle, double *a_distance, double *b_distance, double robot_x, double robot_y, double mid_x, double mid_y, double x, double y);
+//combines functions line from mid and point from robot
+void get_triangle(double *point_x, double *point_y, double robot_x, double robot_y, double x1, double y1, double x2, double y2);
+
+#endif
