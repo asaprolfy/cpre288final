cbfba4c3a3b81d2b5b43e8ed4808188456508ec7
zhala <zhala@iastate.edu>
2019-04-26 14:39:07 -0500

    Clean up sweep

--- project/sweep.c
+++ project/sweep.c
@@ -16,113 +16,10 @@
 #include "robot_position.h"
 
 object_info object[50];
-int count = 0;
-
-// All this stuff is not for the final except for sweep()
-
-/*
-void sweep_and_detect_objects() {
-
-    struct object_info object[3];
-    int degrees, start, finish, flag = 0;
-    double ir, sonar;
-    int count = 0;
-    int i;
-    // sweep
-    for(i = 0; i < 181; i++){
-        degrees = i;
-        sonar = ping_getDistance();
-//        ir = 105375 * pow(adc_avg_distance(), -1.153);
-		ir = pow(adc_avg_distance(), -.885) * 22239;
-
-        // if there is something within range begin keeping track of degrees
-        if(abs(sonar-ir) < 20 && flag == 0 && sonar < 60){
-            flag = 1;
-            start = degrees;
-            object[count].distance = sonar; //nick and I found it is better to measure distance at start of the sweep
-            //count++;
-        }
-        // if the object is no longer detected calculate range and size, add to struct array
-        if(abs(sonar-ir) > 20 && flag == 1 && sonar <60 && ir>sonar){
-            finish = degrees;
-            flag = 0;
-            object[count].degree = finish-start;
-            object[count].width = sqrt(2*pow(sonar,2)-2*pow(sonar,2)*cos((finish-start)*(M_PI/180))) - 3;
-            count++;
-            printf("%d\t%d\n", start, finish);
-        }
-        servo_move(i);
-
-    }
-    lcd_printf("\ndistance\tnum_degree\twidth\n");
-    // try to determine smallest object
-    int smallest_index = 0;
-    double smallest_width = 0;
-    for(i=0; i<count; i++){
-        if((smallest_width == 0) && (object[i].width > 1)){
-            smallest_width = object[i].width;
-        }
-        if((object[i].width < smallest_width) && (object[i].width > 1)){
-            smallest_width = object[i].width;
-            smallest_index = i;
-        }
-    }
-    // if smallest was found print it
-    if(count){
-        lcd_printf("Dist: %.2f width degrees: %d width cm: %.2f count: %d smallest: %d", object[smallest_index].distance, object[smallest_index].degree, object[smallest_index].width, count, smallest_index+1);
-    }
-    else{
-        lcd_printf("No objects found");
-    }
-
-
-
-    // attempt to send to putty
-    uart_sendStr("\ndistance\tnum_degree\twidth\n");
-
-}
-*/
-/*
-void checkpoint1_sweep() {
-
-    //struct object_info c1_objects[10];
-    int j;
-    int c1_degrees;
-    //int currently_detecting_obj = 0;
-    //int obj_num = 0;
-
-    double ir_distance, sonar_distance;
-
-    //uart_sendStr("Degrees    IR_Dist     Sonar_Dist\n");
-
-    lcd_printf("Degrees\nIR_Dist\nSonar_Dist\n");
-    uart_sendStr("Degrees, IR_Dist, Sonar_Dist");
-
-    // sweep and send each degree to putty
-    for(j = 0; j < 181; j += 2) {
-        c1_degrees = j;
-
-        //ir_distance = pow(adc_avg_distance(), -1.855) * 2E+07;
-        ir_distance = 105375 * pow(adc_avg_distance(), -1.153);
-        sonar_distance = ping_getDistance();
-
-        lcd_printf("%d\n%f\n%f\n", c1_degrees, ir_distance, sonar_distance);
-        char str[30];
-        sprintf(str, "%d, %f, %f", c1_degrees, ir_distance, sonar_distance);
-        uart_sendStr(str);
-
-        servo_move(j);
-
-        timer_waitMillis(200);
-    }
-
-    servo_move(0);
-}
-
-*/
+int object_count = 0;
 
 // Don't delete this please gamres
-// Sweeps for objects and returns an integer of the amount of object detected
+// Sweeps for objects and returns an integer of the amount of objects detected
 void sweep(){
     int degrees, start, finish, flag = 0;
     double ir, sonar;
@@ -132,7 +29,8 @@ void sweep(){
     for(i = 0; i < 181; i++){
         degrees = i;
         sonar = ping_getDistance();
-        ir = pow(adc_avg_distance(), -1.855) * 2E+07;
+        // specific to bot 6
+        ir = pow(adc_avg_distance(), -.885) * 22239;
 
         // if there is something within range begin keeping track of degrees
         if(abs(sonar-ir) < 20 && flag == 0 && sonar < 60){
@@ -156,10 +54,12 @@ void sweep(){
 
     }
 
+    object_count += count;
+
 }
 
 int get_num_objects(){
-    return count;
+    return object_count;
 }
 
 void get_object_position(double *x, double *y, int index){
