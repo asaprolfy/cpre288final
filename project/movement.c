f6fbcfab9f8090e4633bdb2730ffe9c2b62e7acc
egesko <49740123+egesko@users.noreply.github.com>
2019-04-28 17:59:24 -0500

    Update movement.c

--- project/movement.c
+++ project/movement.c
@@ -24,31 +24,17 @@ volatile int rawDistance;
 volatile int completion;
 volatile int sense;
 
-/*
- * Notes on how the encoder increments
- *The cumulative number of raw left encoder counts is returned as a signed 16-bit number,
- *The  high byte first. This number will roll over if it passes the max value (at approx. 14.5 meters).
- *Range: -32768 - 32767 counts
- *NOTE: These encoders are square wave, not quadrature, so they rely on the robot’s commanded velocity direction to know when to count up/down.
- *So if the robot is trying to drive forward, and you force the iRobot® Roomba Open Interface (OI) Specification iRobot to spin in reverse,
- *the encoders will count up, (and vice-versa).  Additionally, the encoders will count up when the commanded velocity is zero and the wheels spin.
- *To convert counts to distance, simply do a unit conversion using the equation for circle circumference.
- *N counts * (mm in 1 wheel revolution / counts in 1 wheel revolution) = mm
- *N counts * (pi * 72.0 / 508.8) = mm
- */
 
 void move_forward(oi_t *sensor, int centimeters) //UPDATED to raw data
 {
     completion=1;
     int left = sensor->leftEncoderCount;
-    //int right = sensor -> rightEncoderCount; kinda unnecessary REMOVAL?
 
     int bumpL = 0;
     int bumpR = 0;
 
     int isBoundaryOrCliff = 0;
 
-    //int milli=((centimeters*10)*0.37); old way to determine the distance to travel REMOVAL?
     int sum = 0;
     sum = centimeters;
     oi_setWheels(100, 100); //move forward full speed
@@ -90,7 +76,6 @@ void move_forward(oi_t *sensor, int centimeters) //UPDATED to raw data
 
     }
     update_robot_angle(sum);
-    //lcd_printf("left:%d \n right:%d",(sensor -> leftEncoderCount)-left,(sensor -> rightEncoderCount)-right); REMOVAL?
     oi_setWheels(0, 0); //stop
 
 }
@@ -100,7 +85,7 @@ void move_forward(oi_t *sensor, int centimeters) //UPDATED to raw data
  */
 void turn_right(oi_t *sensor, int degrees)
 {
-    //int sum = degrees; REMOVAL?
+
     int left = sensor->leftEncoderCount;
     //int right = sensor -> rightEncoderCount; not really necessary would allow more accurate turning, accurate to the point of not relevant REMOVAL?
     oi_setWheels(-100, 100); //Opposing directions allows the robot to just turn on its axis, lower values increase accuracy
@@ -108,7 +93,6 @@ void turn_right(oi_t *sensor, int degrees)
     while ((left - sensor->leftEncoderCount) > -390) //sum + CYBOT_TOLERANCE_VALUE_TURN > 0
     {
         oi_update(sensor);
-        lcd_printf("sum:%d\n", left - sensor->leftEncoderCount);
     }
 
     update_robot_angle(degrees);
@@ -134,20 +118,15 @@ void move_backward(oi_t *sensor, int centimeters)  //UPDATED to raw data
 void turn_left(oi_t *sensor, int degrees)
 {
 
-    int sum = 0;
+    int left = sensor->leftEncoderCount;
 
     oi_setWheels(100, -100); //Opposing directions allows the robot to just turn on its axis, lower values increase accuracy
 
-    while (sum + CYBOT_TOLERANCE_VALUE_TURN < degrees)
+    while ((left - sensor->leftEncoderCount) < 390)
     { // Turns the robot until the angle has been met, the constant corrects the turns
 
         oi_update(sensor);
-        sum += sensor->angle;
-        lcd_printf("sum:%d\naim:%d", sum + CYBOT_TOLERANCE_VALUE_TURN, degrees);
     }
-
-    lcd_printf("sum:%d\naim:%d", sum + CYBOT_TOLERANCE_VALUE_TURN, degrees);
-
     update_robot_angle(degrees);
     oi_setWheels(0, 0); //stop
 
