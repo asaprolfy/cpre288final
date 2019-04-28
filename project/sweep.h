b6110210455d3b9e66a9d748ecb8c0477c909f68
egesko <49740123+egesko@users.noreply.github.com>
2019-04-28 17:49:42 -0500

    Update sweep.h

--- project/sweep.h
+++ project/sweep.h
@@ -1,17 +1,18 @@
 #ifndef SWEEP_H_
 #define SWEEP_H_
 
-typedef struct object_information{
+typedef struct object_information
+{
     double distance;
     int degree;
     double width;
     double x;
     double y;
-}object_info;
+} object_info;
 
 void sweep();
 int get_num_objects();
 void get_object_info(double *x, double *y, double *width, int index);
-void add_object(double x, double y, double width);
+void add_object(double width);
 
 #endif /* SWEEP_H_ */
