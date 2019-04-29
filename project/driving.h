c835251fd947af01a1856c5225e73cd8010bd0ba
BJohnson98 <47310376+BJohnson98@users.noreply.github.com>
2019-04-28 21:07:13 -0500

    Update driving.h

--- project/driving.h
+++ project/driving.h
@@ -2,6 +2,27 @@
 #define DRIVING_H_
 
 
-bool drive_through_objects(double x1, double y1, double x2, double y2)
+
+typedef struct driving_objects{
+    double x;
+    double y;
+    double width;
+}info;
+
+
+
+void driver_main();
+
+bool drive_through_objects(int index1, int index2);
+
+void fill_object_struct();
+
+void two_closest_objects(int *index1, int *index2);
+
+void find_objects_in_view(int *number);
+
+bool drive_around_object(double x, double y, double width);
+
+
 
 #endif /* DRIVING_H_ */
