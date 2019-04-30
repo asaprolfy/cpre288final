e8a2c3cf66d6aed7d54bbcfe17402bb5fe10d465
egesko <49740123+egesko@users.noreply.github.com>
2019-04-29 21:55:37 -0500

    Update servo.c

--- project/servo.c
+++ project/servo.c
@@ -1,153 +1,145 @@
 /*
- * servo.c
+ * sweep.c
  *
- *  Created on: Mar 29, 2019
- *      Author: zhala
+ *
+ * Created:  April 17, 2019
+ *   Author: Rolf Anderson, Zane Hala
  */
-# include "servo.h"
 
+#include "sweep.h"
+#include "uart.h"
+#include "adc.h"
+#include <math.h>
+#include "ping.h"
+#include "servo.h"
+#include "geometry.h"
+#include "robot_position.h"
+#include "cliffSensor.h"
 
+object_info object[50];
+int object_count = 0;
 
-void servo_init(){
+// Don't delete this please gamers
+// Sweeps for objects and returns an integer of the amount of objects detected
+void sweep()
+{
+    int count = object_count;
+    int degrees, start, finish, flag = 0;
+    double ir, sonar;
+    double x_obj, y_obj;
+    int i, mid_angle;
 
-    //pulse_width = 0;
+    for (i = 0; i < 181; i++)
+    {
+        degrees = i;
+        sonar = ping_getDistance();
+        // specific to bot 6
+        ir = pow(adc_avg_distance(), -.885) * 22239;
 
-    // turn on clock to PB5
-    SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R1;
-
-    // turn on alt func for port b
-    GPIO_PORTB_AFSEL_R |= 0x20;
-
-    // turn on t1ccp1 pin
-    GPIO_PORTB_PCTL_R |= 0x700000;
-
-    // pb5 output
-    GPIO_PORTB_DIR_R |= 0x20;
-
-    // enable pb5 to be digital
-    GPIO_PORTB_DEN_R |= 0x20;
-
-    // enable timer 1 clock
-    SYSCTL_RCGCTIMER_R |= SYSCTL_RCGCTIMER_R1;
-
-    // disable timer 1b to configure
-    TIMER1_CTL_R &= ~(TIMER_CTL_TBEN);
-
-    // set it to 16 bit mode
-    TIMER1_CFG_R |= 0x00000004;
-
-    // set periodic mode
-    TIMER1_TBMR_R |= 0x0A;
-
-    //periodic and PWM enable
-    TIMER1_TBMR_R |= (TIMER_TBMR_TBMR_PERIOD | TIMER_TBMR_TBAMS);
-    TIMER1_TBMR_R &= ~(TIMER_TBMR_TBCMR);
-
-    //set to non-inverted PWM mode
-    TIMER1_CTL_R &= ~(TIMER_CTL_TBPWML);
-
-    // set lower 16 bits of interval register with period
-    TIMER1_TBILR_R |= (PERIOD & 0xFFFF);
-
-    // set upper 8 bits of interval register with period
-    TIMER1_TBPR_R |= (PERIOD >> 16);
-
-    pulse_width = 0;
-    servo_move_to_zero();
-
-    // set lower 16 bits of timer b match with low pulse width
-    TIMER1_TBMATCHR_R = ((PERIOD - pulse_width) & 0xFFFF);
-
-    //set upper 8 bits of timer b prescale match with high pulse width
-    TIMER1_TBPMR_R = ((PERIOD - pulse_width) >> 16);
-
-    // enable timer 1b after config
-    TIMER1_CTL_R |= 0x0100;
-
-    timer_waitMillis(1000);
-}
-
-int servo_move(float degree){
-    if (degree < 0) {
-        return 0;
-    }
-
-    if (degree > 180) {
-        return 180;
-    }
-    pulse_width = -157.9 * degree + 30044;
-
-    //set lower 16 bits of pulse width
-    //TIMER1_TBMATCHR_R = ((320000 - pulse_width) & 0xFFFF);
-    TIMER1_TBMATCHR_R = ((320000 - pulse_width) & 0xFFFF);
-
-    //set the upper 8 bits of the pulse width
-    TIMER1_TBPMR_R |= ((320000 - pulse_width) >> 16);
-    //TIMER1_TBPMR_R |= ((320000 - pulse_width) & 0xFFFF);
-
-    //Delay for the servo to move to the position
-    timer_waitMillis(50);
-
-    return pulse_width;
-}
-
-void servo_move_to_zero(){
-
-    //pulse_width = 30000;
-
-    pulse_width = 29944;
-
-    //set lower 16 bits of pulse width
-    //TIMER1_TBMATCHR_R = ((320000 - pulse_width) & 0xFFFF);
-    TIMER1_TBMATCHR_R = ((320000 - pulse_width) & 0xFFFF);
-
-    //set the upper 8 bits of the pulse width
-    //TIMER1_TBPMR_R |= ((320000 - pulse_width) >> 16);
-    TIMER1_TBPMR_R |= ((320000 - pulse_width) & 0xFFFF);
-
-    //Delay for the servo to move to the position
-}
-
-void checkpoint2(){
-    float degree = 90;
-    int direction = 1;
-    int p;
-
-    servo_move(90);
-
-    while(1){
-        switch(button_getButton()) {
-        case 1:
-            degree = degree + (1 * direction);
-            p = servo_move(degree);
-            lcd_printf("%f %d %d", degree, p, direction);
-            break;
-        case 2:
-            degree = degree + (2.5 * direction);
-            p = servo_move(degree);
-            lcd_printf("%f %d %d", degree, p, direction);
-            break;
-        case 3:
-            degree = degree + (5 * direction);
-            servo_move(degree);
-            lcd_printf("%f %d %d", degree, p, direction);
-            break;
-        case 4:
-            direction = direction * -1;
-            lcd_printf("%f %d %d", degree, p, direction);
-            break;
-        case 5:
-            degree = 0;
-            p = servo_move(degree);
-            lcd_printf("%f %d %d", degree, p, direction);
-            break;
-        case 6:
-            degree = 180;
-            p = servo_move(degree);
-            lcd_printf("%f %d %d", degree, p, direction);
-            break;
+        // if there is something within range begin keeping track of degrees
+        if (abs(sonar - ir) < 20 && flag == 0 && sonar < 60)
+        {
+            flag = 1;
+            start = degrees;
         }
+        // if the object is no longer detected calculate range and size, add to struct array
+        if (abs(sonar - ir) > 20 && flag == 1 && sonar < 60 && ir > sonar)
+        {
+            finish = degrees;
+            flag = 0;
+            // put in the struct given
+            mid_angle = (start + finish) / 2;
+            object_position(mid_angle, sonar, &x_obj, &y_obj);
+            object[count].distance = sonar;
+            object[count].width = sqrt(
+                    2 * pow(sonar, 2)
+                            - 2 * pow(sonar, 2)
+                                    * cos((finish - start) * (M_PI / 180))) - 3;
+            object[count].x = x_obj;
+            object[count].y = y_obj + (object[count].width / 2);
+            count++;
+        }
+        servo_move(i);
+
     }
+
+    object_count += count;
 
 }
 
+int get_num_objects()
+{
+    return object_count;
+}
+
+void get_object_info(double *x, double *y, double *width, int index)
+{
+
+    *x = object[index].x;
+    *y = object[index].y;
+    *width = object[index].width;
+}
+
+void add_object(double width)
+{ //Removed the x,y function parameters and moved robot x,y to being called in the function (Asa)
+    double x,y;
+    get_robot_position(&x,&y);
+    object_count += 1;
+    object[object_count].x = x;
+    object[object_count].y = y;
+    object[object_count].width = width;
+    double angle = get_robot_angle(); //Allows for the direction the robot is moving to be realized (Asa)
+    /*
+     *  sense allows the robot to determine on which side of the robot is the object it hit. '5' left side, '6' is the right side.
+     * Somewhat arbitrary distances are based off a sketch of a 45 degree path in the quadrant, applies to all the "math" (Asa)
+     *
+     */
+    if (sense == 5)
+    {
+        if (angle > 0 && angle < 90) //Q1
+        {
+            object[object_count].x = x;
+            object[object_count].y = y + 5;
+        }
+        else if (angle >= 90 && angle < 180) //Q2
+        {
+            object[object_count].x = x-5;
+            object[object_count].y = y;
+        }
+        else if (angle >= 180 && angle < 270) //Q3
+        {
+            object[object_count].x = x;
+            object[object_count].y = y - 5;
+        }
+        else if (angle >= 270 && (angle <= 360 || angle == 0)) //Q4
+        {
+            object[object_count].x = x + 5;
+            object[object_count].y = y;
+        }
+        object[object_count].width = 13;
+    }
+    else if (sense == 6)
+    {
+        if (angle > 0 && angle <= 90) //Q1
+        {
+            object[object_count].x = x + 5;
+            object[object_count].y = y;
+        }
+        else if (angle > 90 && angle <= 180) //Q2
+        {
+            object[object_count].x = x;
+            object[object_count].y = y + 5;
+        }
+        else if (angle > 180 && angle <= 270) //Q3
+        {
+            object[object_count].x = x-5;
+            object[object_count].y = y;
+        }
+        else if (angle > 270 && (angle <= 360 || angle == 0)) //Q4
+        {
+            object[object_count].x = x;
+            object[object_count].y = y-5;
+        }
+        object[object_count].width = 13;
+    }
+}
