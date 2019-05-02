ef9b796000678d504ac27a18af1a86897fb8e492
BJohnson98 <47310376+BJohnson98@users.noreply.github.com>
2019-05-02 14:00:47 -0500

    Update sweep.h

--- project/sweep.h
+++ project/sweep.h
@@ -1,19 +1,29 @@
+/*
+ * sweep.h
+ *
+ *
+ * Created:  April 17, 2019
+ *   Author: Rolf Anderson
+ */
+
 #ifndef SWEEP_H_
 #define SWEEP_H_
 
-typedef struct object_information
-{
+
+typedef struct object_information{
     double distance;
     int degree;
     double width;
     double x;
     double y;
-} object_info;
+}object_info;
+
+
+volatile extern object_info object[50];
 
 void sweep();
 int get_num_objects();
 void get_object_info(double *x, double *y, double *width, int index);
-void add_object(double width);
-int check_existing_object(double x, double y, double width);
+int check_existing_object(double x, double y);
 
 #endif /* SWEEP_H_ */
