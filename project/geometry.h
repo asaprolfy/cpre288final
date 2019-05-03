261a467073e89930716c0dc8509b1d7b7f4bf3a2
BJohnson98 <47310376+BJohnson98@users.noreply.github.com>
2019-05-02 22:16:32 -0500

    Update geometry.h

--- project/geometry.h
+++ project/geometry.h
@@ -20,5 +20,9 @@ void get_triangle(double *point_x, double *point_y, double x1, double y1, double
 double min_angle(double object_width, double object_distance);
 //returns 1 if the robot sees the object, 0 if it doesnt.
 int in_view(double x, double y);
+//drives the robot to a given x,y point.
+void drive_to_point(int *direction, double *length, double *angle, double x, double y);
+//finds a point near the object, used to drive around an object
+void point_near_object(double *x, double *y, double x_obj, double y_obj, double width, int side);
 
 #endif
